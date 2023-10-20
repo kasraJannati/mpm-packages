228c9c77f02ce3c0b169038ae23ff15d35f8bcfe
Kasra <kasra.ja1991@gmail.com>
2023-10-20 16:20:13 -0400

    fix: update workflows

--- release.config.cjs
+++ release.config.cjs
@@ -4,22 +4,21 @@
 module.exports = {
     branches: ["master"],
     plugins: [
-        '@semantic-release/commit-analyzer', // It will increment the version for us. Determine the type of release by analyzing commits
-        //: This plugin analyzes the commit messages and determines the type of release (major, minor, or patch) based on conventional commit message format.
-        '@semantic-release/release-notes-generator',// Generate release notes for the commits added 
-        //It generates release notes based on the commits added to your repository, which can be useful for creating changelogs.
-        '@semantic-release/github', // it creates the release itself, also creates the tag with the correct verion for us 
-        //: This plugin handles the creation of releases on GitHub, including tagging with the correct version.
-
+        // It will increment the version. This plugin analyzes the commit messages and determines the type of release (major, minor, or patch) based on conventional commit message format.
+        '@semantic-release/commit-analyzer', 
+        // It generates release notes based on the commits added to your repository, which can be useful for creating changelogs.
+        '@semantic-release/release-notes-generator',
+        // This plugin handles the creation of releases on GitHub, including tagging with the correct version.
+        '@semantic-release/github',
+        // This plugin is responsible for releasing new versions of the package on the npm registry. 
         "@semantic-release/npm",
         {
             "pkgRoot": "dist"
         },
-
+        // This plugin is responsible for updating the package.json file with the new version. It is also configured to commit the updated package.json file during the release process.
         "@semantic-release/git",
         {
           "assets": ["package.json"]
         }
-
     ]
   };
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,1 @@
 export * from './BaseIntComponent'; 
-export * from './newComponent'; 
