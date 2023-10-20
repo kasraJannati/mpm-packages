8c042d97ee427b0b959919420926d8a903e31d30
Kasra <kasra.ja1991@gmail.com>
2023-10-20 15:17:23 -0400

    feat: remove comment

--- release.config.cjs
+++ release.config.cjs
@@ -6,5 +6,11 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer', // It will increment the version for us. Determine the type of release by analyzing commits
         '@semantic-release/release-notes-generator',// Generate release notes for the commits added 
+            [
+                '@semantic-release/github',
+                {
+                    assets: 'dist/*', // You may need to specify the assets to upload
+                }
+            ]
     ]
   };
