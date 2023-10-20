010bb65d7096c35beec07f40bf62a90a2eb9cced
Kasra <kasra.ja1991@gmail.com>
2023-10-20 15:20:41 -0400

    chore(release): Version 2.1.0

--- release.config.cjs
+++ release.config.cjs
@@ -6,11 +6,6 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer', // It will increment the version for us. Determine the type of release by analyzing commits
         '@semantic-release/release-notes-generator',// Generate release notes for the commits added 
-            [
-                '@semantic-release/github',
-                {
-                    assets: 'dist/*', // You may need to specify the assets to upload
-                }
-            ]
+        '@semantic-release/github' // it creates the release itself, also creates the tag with the correct verion for us 
     ]
   };
