f59ef5c8579c01266e2e767214a841cf2b998dfd
Kasra <kasra.ja1991@gmail.com>
2023-10-24 13:42:10 -0400

    fix: remove

--- src/newComponent.tsx
+++
@@ -1,5 +0,0 @@
-export const getTodaysDateOne = () => {
-    const today = new Date();
-    const options:any = { year: 'numeric', month: 'long', day: 'numeric' };
-    return today.toLocaleDateString('en-US', options);
-} 
